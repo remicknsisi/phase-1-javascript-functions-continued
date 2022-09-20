dbda1320d820fa5df2e771b0e36a8269dccfd9cf
Sienna <remicknsisi@gmail.com>
2022-09-20 17:23:13 -0400

    commit tree d53a2f49f05c581c9e3350a8c9eebc34f120cb16

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (activity === undefined) {activity = 'roller-skate'}
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity) {
+    if (activity === undefined) {activity = 'go to the office'}
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(addedSymbol){
+    if (addedSymbol === undefined) {addedSymbol = '*'}
+    return function (adjective){
+        if (adjective === undefined) {adjective = 'special'}
+        return `You are ${addedSymbol}${adjective}${addedSymbol}!`
+    }
+}
